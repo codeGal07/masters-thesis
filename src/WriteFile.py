a78d7d5322ad5ddd82d2c2591d43c57de435e76a
sabina <sabina.matjasic@gmail.com>
2023-06-11 20:55:49 +0200

    Define more decimals in polarity and rename variable

--- src/WriteFile.py
+++ src/WriteFile.py
@@ -1,17 +1,17 @@
 import os
 
 
-def write_data_into_file(date, stock_name, polarity, source, file_data_path):
+def write_data_into_file(date, stock_name, polarity, source, current_url, file_data_path):
     with open(file_data_path, "a") as file:
-        new_data_row = "{}, {}, {:.1f}, {}".format(date, stock_name, polarity, source)
+        new_data_row = "{}, {}, {:.5f}, {}, {}".format(date, stock_name, polarity, source, current_url)
         file.write(new_data_row + "\n")
 
 
-def create_file_if_not_exists(file_path):
-    if not os.path.exists(file_path):
-        with open(file_path, "w") as file:
-            header = "date, stock_name, polarity, source\n"
+def create_file_if_not_exists(file_data_path):
+    if not os.path.exists(file_data_path):
+        with open(file_data_path, "w") as file:
+            header = "date, stock_name, polarity, source, url\n"
             file.write(header)
-            print("New file created at:", file_path)
+            print("New file created at:", file_data_path)
     else:
-        print("File already exists at:", file_path)
+        print("File already exists at:", file_data_path)
