08d66e3387a603a277d0198aef56f5484d2d2000
sabina <sabina.matjasic@gmail.com>
2023-06-11 20:55:13 +0200

    Wrap code in try catch block

--- src/YahooFinance/HelperMethodsYahooFinance.py
+++ src/YahooFinance/HelperMethodsYahooFinance.py
@@ -3,8 +3,11 @@ from selenium.webdriver.common.by import By
 
 
 def click_agree_button_yahoo_finance(driver):
-    agree_button_yahoo = driver.find_element(By.NAME, 'agree')
-    agree_button_yahoo.click()
+    try:
+        agree_button_yahoo = driver.find_element(By.NAME, 'agree')
+        agree_button_yahoo.click()
+    except:
+        pass
 
 
 def click_show_more_button_yahoo_finance(driver):
