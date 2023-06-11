054382f85c4df8c172786aed902fd5c9237619bb
sabina <sabina.matjasic@gmail.com>
2023-06-11 20:53:29 +0200

    Add headless mode of webdriver and some smaller changes

--- src/GetData.py
+++ src/GetData.py
@@ -8,7 +8,7 @@ from src.WriteFile import *
 from src.YahooFinance.HelperMethodsYahooFinance import *
 
 
-def get_all_data(date_from, date_to, number_of_hits, file_data_path):
+def get_all_data(date_from, date_to, number_of_hits, file_data_path, headless):
     trading_dates = get_trading_dates(date_from, date_to)
     stock_symbols = get_all_stock_symbols()
     sources = get_specify_sources()
@@ -18,7 +18,7 @@ def get_all_data(date_from, date_to, number_of_hits, file_data_path):
         after = trading_dates[i + 1].strftime("%Y-%m-%d")
         for stock_name in stock_symbols:
             for source in sources:
-                search_stock_info(stock_name, source, before, after, number_of_hits, file_data_path)
+                search_stock_info(stock_name, source, before, after, number_of_hits, file_data_path, headless)
 
 
 def get_trading_dates(start_date, end_date):
@@ -34,8 +34,9 @@ def get_trading_dates(start_date, end_date):
 
 def get_all_stock_symbols():
     # Read the CSV file into a pandas DataFrame
-    data = pd.read_csv('data/sp500_data.csv')
-    stock_symbols = data['Symbol']
+    # TODO temporary cropped data
+    data = pd.read_csv('data/sp500_CROP.csv')
+    stock_symbols = data['Company']
 
     return stock_symbols
 
@@ -48,11 +49,16 @@ def get_specify_sources():
     return sources
 
 
-def search_stock_info(stock_name, source, after, before, number_of_hits, file_data_path):
+def search_stock_info(stock_name, source, after, before, number_of_hits, file_data_path, headless):
     search_string = stock_name + " site:" + source + " after:" + after + " before:" + before
 
-    driver = webdriver.Chrome('chromedriver')
-    driver.set_window_position(-1000, 0)
+    if headless:
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome('chromedriver', options=options)
+    else:
+        driver = webdriver.Chrome('chromedriver')
+        driver.set_window_position(-1000, 0)
 
     for i in range(number_of_hits):
         driver.get("https://www.google.com/search?q=" + search_string + "&start=" + str(i))
@@ -60,10 +66,12 @@ def search_stock_info(stock_name, source, after, before, number_of_hits, file_da
         if i == 0:
             agree_button_google = driver.find_element(By.ID, 'L2AGLb')
             agree_button_google.click()
+            time.sleep(5)
 
         # Click the first hit on Google
         first_hit = driver.find_element(By.CLASS_NAME, 'MjjYud')
         first_hit.click()
+        time.sleep(5)
 
         # Get text data based on website
         text_data = ""
@@ -74,21 +82,23 @@ def search_stock_info(stock_name, source, after, before, number_of_hits, file_da
 
         polarity = evaluate_text_semantics(text_data)
 
-        write_data_into_file(before, stock_name, polarity, source, file_data_path)
+        current_url = driver.current_url
+        write_data_into_file(before, stock_name, polarity, source, current_url, file_data_path)
 
     driver.quit()
 
 
 def main():
-    date_from = '2023-02-01'
+    date_from = '2023-02-07'
     date_to = '2023-03-01'
-    number_of_hits = 1
+    number_of_hits = 5
     file_data_path = "data/stock_info.txt"
+    headless = True
 
     # Only needed once to get SP500 data into CSV file
     # get_SP500_data()
-
-    get_all_data(date_from, date_to, number_of_hits, file_data_path)
+    create_file_if_not_exists(file_data_path)
+    get_all_data(date_from, date_to, number_of_hits, file_data_path, headless)
 
 
 if __name__ == "__main__":
