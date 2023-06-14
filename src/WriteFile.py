46a022f47902f91bc5df3a4aeb05ef8e2e7ae297
sabina <sabina.matjasic@gmail.com>
2023-06-14 15:59:35 +0200

    Write title of the website in the file

--- src/WriteFile.py
+++ src/WriteFile.py
@@ -1,16 +1,16 @@
 import os
 
 
-def write_data_into_file(date, stock_name, polarity, source, current_url, file_data_path):
+def write_data_into_file(date, stock_name, polarity, source, title_data, current_url, file_data_path):
     with open(file_data_path, "a") as file:
-        new_data_row = "{}, {}, {:.5f}, {}, {}".format(date, stock_name, polarity, source, current_url)
+        new_data_row = "{}, {}, {:.5f}, {}, {}, {}".format(date, stock_name, polarity, title_data, source, current_url)
         file.write(new_data_row + "\n")
 
 
 def create_file_if_not_exists(file_data_path):
     if not os.path.exists(file_data_path):
         with open(file_data_path, "w") as file:
-            header = "date, stock_name, polarity, source, url\n"
+            header = "date, stock_name, polarity, title_data, source, url\n"
             file.write(header)
             print("New file created at:", file_data_path)
     else:
--- src/YahooFinance/HelperMethodsYahooFinance.py
+++ src/YahooFinance/HelperMethodsYahooFinance.py
@@ -5,8 +5,11 @@ from selenium.webdriver.common.by import By
 def click_agree_button_yahoo_finance(driver):
     try:
         agree_button_yahoo = driver.find_element(By.NAME, 'agree')
+        # agree_button_yahoo = driver.find_element(By.CSS_SELECTOR, 'button.btn.secondary.accept-all')
         agree_button_yahoo.click()
+
     except:
+
         pass
 
 
@@ -17,6 +20,13 @@ def click_show_more_button_yahoo_finance(driver):
     except NoSuchElementException:
         pass
 
+
+def get_title_yahoo_finance(driver):
+    try:
+        title_yahoo = driver.find_element(By.CSS_SELECTOR, "div.caas-title-wrapper")
+        return title_yahoo.text
+    except NoSuchElementException:
+        pass
 
 def get_data_yahoo_finance(driver):
     try:
